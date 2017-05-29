0cfef537773f35b77cd0dd888801c6ff8c0e7fa2
ELISANDRO MELLO CORREA <ELISANDRO MELLO CORREA>
2017-05-28 22:31:16 -0300

    Desenvolvimneto View e Controller Página Usuario

---
+++ app/controllers/UsuarioController.php
@@ -0,0 +1,47 @@
+<?php 
+
+class UsuarioController extends \HXPHP\System\Controller
+{
+	
+    public function indexAction()
+    {
+        $this->view->setHeader('usuario/header')
+               ->setFooter('usuario/footer');
+    }
+
+	public function __construct($configs)
+	{
+		parent::__construct($configs);
+
+		$this->load(
+			'Services\Auth',
+			$configs->auth->after_login,
+			$configs->auth->after_logout,
+			true
+		);
+
+		$this->auth->redirectCheck();
+		$this->auth->roleCheck(array(
+			'administrator'
+		));
+
+		$user_id = $this->auth->getUserId();
+		$user = User::find($user_id);
+		$role = Role::find($user->role_id);
+
+		$this->load(
+			'Helpers\Menu',
+			$this->request,
+			$this->configs,
+			$role->role
+		);
+
+		$this->view->setTitle('HXPHP - Administrativo')
+					->setFile('index')
+					->setVars([
+						'user' => $user,
+						'users' => User::all()
+					]);
+	}
+
+}
